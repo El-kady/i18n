fffb509efe065cbc94f4f0136b3c5e7079b711e5
Moustafa Elkady <Moustafa.m.elkady@gmail.com>
2024-05-15 09:28:27 +0300

    Create ar.js

---
+++ locale/ar.js
@@ -0,0 +1,20 @@
+{
+  "EPP_Common": {
+    "YouCanConvertMsg": "يمكنك تحويل هذه المعاملة إلى أقساط الآن! وبفائدة تبدأ من {{rate}}% فقط*. مع إمكانية السداد على فترات مختلفة.",
+    "Convert": "تحويل إلى أقساط.",
+    "Error": "عفوا يوجد خطأ ما!",
+    "UnableToProcess": "لا يمكن إرسال طلبك في الوقت الحالي. يرجى إعادة المحاولة لاحقًا.",
+    "TransactionDetails": "تفاصيل المعاملة:",
+    "OutstandingAmount": "المبلغ المستحق:",
+    "MerchantName": "اسم التاجر:",
+    "PrincipalAmount": "المبلغ الأصلي:",
+    "InterestRate": "معدل الفائدة:",
+    "InstallmentStartDate": "تاريخ بدء التقسيط:",
+    "InstallmentDetails": "تفاصيل القسط:",
+    "RemainingInstallments": "الأقساط المتبقية: {{remaining}} من {{total}}  أشهر.",
+    "MaturityDate": "تاريخ الاستحقاق:",
+    "PrincipalInstallmentAmount": "مبلغ القسط الأساسي:",
+    "InstallmentAmountInterest": "فائدة مبلغ القسط:",
+    "InstallmentDueDate": "تاريخ استحقاق القسط:"
+  }
+}
